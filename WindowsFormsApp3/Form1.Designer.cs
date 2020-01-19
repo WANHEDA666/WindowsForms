3353e614cb4371567171c82e2c05558f2c172ebf
WANHEDA666 <48631287+WANHEDA666@users.noreply.github.com>
2020-01-19 19:17:54 +0300

    add files

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp3 {
+	partial class Form1 {
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent() {
+			this.button2 = new System.Windows.Forms.Button();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(822, 475);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(145, 69);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "Построить график";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2_Click);
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.panel2.Location = new System.Drawing.Point(12, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(778, 532);
+			this.panel2.TabIndex = 1;
+			this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel2_Paint_1);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(822, 385);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(145, 68);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Сбросить построение";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(929, 74);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(38, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(809, 68);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(102, 26);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Введите значение \r\nпараметра a";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(834, 36);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(97, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "y^2  = x^3/(2*a - x)";
+			this.label2.Click += new System.EventHandler(this.Label2_Click);
+			// 
+			// Form1
+			// 
+			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.ClientSize = new System.Drawing.Size(979, 556);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.button2);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "Form1";
+			this.Text = "График функции Циссоида Диокла.";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+	}
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3 {
+	public partial class Form1 : Form {
+		//Пикселей в одном делении оси
+		const int PIX_IN_ONE = 15;
+		//Длина стрелки
+		const int ARR_LEN = 10;
+
+		public Form1() {
+			InitializeComponent();
+			//panel2.Paint += Panel2_Paint;
+		}
+
+
+		private void Button2_Click(object sender, EventArgs e) {
+			int res, a;
+			if (Int32.TryParse(textBox1.Text, out res) && res > 0 && res < 10) {
+				a = res;
+			}
+			else {
+				MessageBox.Show("Неверное значение для параметра а." + " Введите, пожалуйста, целое число, больше 0 и меньше 10.");
+				return;
+			}
+
+			Pen myPen = new Pen(Color.Blue, 1);
+			Graphics g = Graphics.FromHwnd(panel2.Handle);
+
+			//int a = 5;
+			float x = -3;
+			for (int i = 1; i <= 280; i += 1) {
+				x += 0.1F;
+				float z = (x * x * x / ((2 * a) - x));
+				z = (float)Math.Sqrt(Math.Abs(z));
+				float xx = x;
+				xx = x + 0.1f;
+				float z1 = (xx * xx * xx / ((2 * a) - xx));
+				z1 = (float)Math.Sqrt(Math.Abs(z1));
+				g.DrawLine(myPen, x*20 + 390, z*20+266, xx*20 + 390, z1*20+266);
+				g.DrawLine(myPen, x * 20 + 390, -z * 20 + 266, xx * 20 + 390, -z1 * 20 + 266);
+				System.Threading.Thread.Sleep(5);
+			}
+
+		}
+
+		private void Button3_Click(object sender, EventArgs e) {
+			Application.Restart();
+
+		}
+
+
+
+		//оси координат
+		private void Panel2_Paint_1(object sender, PaintEventArgs e) {
+			int w = panel2.ClientSize.Width / 2;
+			int h = panel2.ClientSize.Height / 2;
+			//Смещение начала координат в центр PictureBox
+			e.Graphics.TranslateTransform(w, h);
+			DrawXAxis(new Point(-w, 0), new Point(w, 0), e.Graphics);
+			DrawYAxis(new Point(0, h), new Point(0, -h), e.Graphics);
+			//Центр координат
+			e.Graphics.FillEllipse(Brushes.Red, -2, -2, 4, 4);
+		}
+
+		//Рисование оси X
+		private void DrawXAxis(Point start, Point end, Graphics g) {
+			//Ось
+			g.DrawLine(Pens.Black, start, end);
+			//Стрелка
+			g.DrawLines(Pens.Black, GetArrow(start.X, start.Y, end.X, end.Y, ARR_LEN));
+		}
+
+		//Рисование оси Y
+		private void DrawYAxis(Point start, Point end, Graphics g) {
+			//Ось
+			g.DrawLine(Pens.Black, start, end);
+			//Стрелка
+			g.DrawLines(Pens.Black, GetArrow(start.X, start.Y, end.X, end.Y, ARR_LEN));
+		}
+
+		//Рисование текста
+		private void DrawText(Point point, string text, Graphics g, bool isYAxis = false) {
+
+		}
+
+		//Вычисление стрелки оси
+		private static PointF[] GetArrow(float x1, float y1, float x2, float y2, float len = 10, float width = 4) {
+			PointF[] result = new PointF[3];
+			//направляющий вектор отрезка
+			var n = new PointF(x2 - x1, y2 - y1);
+			//Длина отрезка
+			var l = (float)Math.Sqrt(n.X * n.X + n.Y * n.Y);
+			//Единичный вектор
+			var v1 = new PointF(n.X / l, n.Y / l);
+			//Длина стрелки
+			n.X = x2 - v1.X * len;
+			n.Y = y2 - v1.Y * len;
+			result[0] = new PointF(n.X + v1.Y * width, n.Y - v1.X * width);
+			result[1] = new PointF(x2, y2);
+			result[2] = new PointF(n.X - v1.Y * width, n.Y + v1.X * width);
+			return result;
+		}
+
+		private void Label2_Click(object sender, EventArgs e) {
+
+		}
+	}
+}
